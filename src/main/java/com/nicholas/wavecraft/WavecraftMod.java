8bbeab223f8fd6263c8ef7d9d415c112173bf35d
nbergquist <100451624@alumnos.uc3m.es>
2025-04-24 16:57:00 +0200

    Versión PC Portátil. /wc renderSPL implementado

--- src/main/java/com/nicholas/wavecraft/WavecraftMod.java
+++ src/main/java/com/nicholas/wavecraft/WavecraftMod.java
@@ -1,4 +1,6 @@
 package com.nicholas.wavecraft;
+import com.nicholas.wavecraft.debug.SoundDebugger;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -7,7 +9,9 @@ import com.nicholas.wavecraft.commands.WavecraftCommand;
 @Mod("wavecraft")
 public class WavecraftMod {
     public WavecraftMod() {
+
         System.out.println("Wavecraft cargado");
+        MinecraftForge.EVENT_BUS.register(SoundDebugger.class);
     }
 
     @Mod.EventBusSubscriber(modid = "wavecraft")
